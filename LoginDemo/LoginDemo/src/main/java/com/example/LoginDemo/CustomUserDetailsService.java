c3c2a9de2c7da8d1df9bae55e9a2cbf141565178
SachPrecious <sachithrabandara49@gmail.com>
2022-05-17 17:10:01 +0530

    web Security configuration

---
+++ LoginDemo/LoginDemo/src/main/java/com/example/LoginDemo/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.example.LoginDemo;
+
+import com.example.LoginDemo.model.AppUser;
+import com.example.LoginDemo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        AppUser appUser=userRepository.findByUsername(username);
+        if (appUser==null){
+            throw new UsernameNotFoundException("User Not Found!");
+        }
+
+        return null;
+    }
+}
